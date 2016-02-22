89b48854c0c525c4b504afbcb4e6466e6ae90a9e
Jared L Jennings <jared@jaredjennings.org>
2016-02-22 14:56:53 -0600

    Fixed a bug with last weeks average
    The results were incorrect returning the results from the previous entry, as like for the daily report.

--- app/helpers/love_report_helper.rb
+++ app/helpers/love_report_helper.rb
@@ -2,14 +2,16 @@ module LoveReportHelper
 
   def average_rating(user)
     @user = user
-    @user.partners_entries.average(:rating)
+    @user.partners_entries.average(:rating).round(2)
   end
 
   def last_weeks_average_rating(user)
     @user = user
-    @entries = @user.partners_entries#.group_by_week(:created_at, last: 2, current: false)#.count.average(:rating)
+    @entries = Entry.where('linked_profile_id =? and received = true and private != true', @user.id)
+    # TODO: This query could get really big since all results are being loaded.
+    # we should only load last weeks results.
     @result = @entries.group_by_week(:created_at, last: 1, current: false).average(:rating)
-    @result.first[1]
+    @result.first[1].round(2)
   end
 
 
