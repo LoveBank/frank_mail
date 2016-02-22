1a4b1b76e7721b756dea6718c1388e91a7979c16
Jared L Jennings <jared@jaredjennings.org>
2016-02-22 15:22:42 -0600

    Missing Module reference

--- app/helpers/love_report_helper.rb
+++ app/helpers/love_report_helper.rb
@@ -7,7 +7,7 @@ module LoveReportHelper
 
   def last_weeks_average_rating(user)
     @user = user
-    @entries = Entry.where('linked_profile_id =? and received = true and private != true', @user.id)
+    @entries = Frank::Entry.where('linked_profile_id =? and received = true and private != true', @user.id)
     # TODO: This query could get really big since all results are being loaded.
     # we should only load last weeks results.
     @result = @entries.group_by_week(:created_at, last: 1, current: false).average(:rating)
