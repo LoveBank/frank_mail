f179b73e616b5550abc8d648755e73b9f808ccef
Jared L Jennings <jared@jaredjennings.org>
2016-02-25 17:47:32 -0600

    Didn't handle empty results in calculations

--- app/helpers/love_report_helper.rb
+++ app/helpers/love_report_helper.rb
@@ -4,14 +4,14 @@ module LoveReportHelper
     @user = user
     @entries = Frank::Entry.where('linked_profile_id =? and received = true and private != true and created_at >= ?',
                                   @user.id, Time.now.beginning_of_week)
-    @entries.average(:rating).round(2)
+    @entries.average(:rating).round(2) unless @entries.nil? || @entries.count == 0
   end
 
   def last_weeks_average_rating(user)
     @user = user
     @entries = Frank::Entry.where('linked_profile_id =? and received = true and private != true and created_at BETWEEN ? and ?',
                                   @user.id, 1.week.ago.beginning_of_week, 1.week.ago.end_of_week)
-    @entries.average(:rating).round(2)
+    @entries.average(:rating).round(2) unless @entries.nil? || @entries.count == 0
   end
 
 
