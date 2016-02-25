f2af4273ed6fbda4864056f60b6a7d93623bdc8a
Jared L Jennings <jared@jaredjennings.org>
2016-02-25 17:43:12 -0600

    Fixed broken summary calculations

--- app/helpers/love_report_helper.rb
+++ app/helpers/love_report_helper.rb
@@ -2,16 +2,16 @@ module LoveReportHelper
 
   def average_rating(user)
     @user = user
-    @user.partners_entries.average(:rating).round(2)
+    @entries = Frank::Entry.where('linked_profile_id =? and received = true and private != true and created_at >= ?',
+                                  @user.id, Time.now.beginning_of_week)
+    @entries.average(:rating).round(2)
   end
 
   def last_weeks_average_rating(user)
     @user = user
-    @entries = Frank::Entry.where('linked_profile_id =? and received = true and private != true', @user.id)
-    # TODO: This query could get really big since all results are being loaded.
-    # we should only load last weeks results.
-    @result = @entries.group_by_week(:created_at, last: 1, current: false).average(:rating)
-    @result.first[1].round(2)
+    @entries = Frank::Entry.where('linked_profile_id =? and received = true and private != true and created_at BETWEEN ? and ?',
+                                  @user.id, 1.week.ago.beginning_of_week, 1.week.ago.end_of_week)
+    @entries.average(:rating).round(2)
   end
 
 
